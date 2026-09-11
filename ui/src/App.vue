29044b9122ca941385d116f7902fa4f11aa2902d
Alexey Razuvaev <32393222+razuv@users.noreply.github.com>
2026-09-11 20:21:08 +0300

    Update App.vue

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -54,8 +54,8 @@ onMounted(reloadApplication);
 @import './assets/styles/main.scss';
 
 .layout {
-  width: 100%;
-  max-width: 1440px;
+  width: 360px;
+  max-width: 100%;
   min-width: 0;
   flex-shrink: 0;
   container: page / inline-size;
@@ -63,6 +63,9 @@ onMounted(reloadApplication);
 }
 
 
+@media (min-width: 720px) { .layout { width:720px; } }
+@media (min-width: 1080px) { .layout { width:1080px; } }
+@media (min-width: 1440px) { .layout { width:1440px; } }
 
 .loading-error {
   min-height: 100vh;
